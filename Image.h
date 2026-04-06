94c5f2daf142d9918ff9d25ede31a188465f4a14
blahova <118811455+blahova@users.noreply.github.com>
2026-04-06 22:55:34 +0200

    anizotropne, robi to co eme, nevieme ci dobre

---
+++ Image.h
@@ -0,0 +1,56 @@
+#pragma once
+#define _USE_MATH_DEFINES
+#include <QVector>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <filesystem>
+#include <random>
+#include <math.h>
+
+//eigen
+#include "Eigen/Sparse"
+#include "Eigen/Dense"
+#include "Eigen/SparseLU"
+
+class Image {
+private:
+	int width = 0;
+	int height = 0;
+	int size = 0;
+
+	std::vector<double> imageData;	//originalny ovrazok
+	std::vector<uint8_t> mask;	//maska
+	std::vector<double> damaged;	//aplikacia masky na obrazok
+	std::vector<double> laplace;	//uz po laplaceovi
+	std::vector<double> smoothed;	//smooth
+
+	//ANIZOTROPNE
+	std::vector<std::vector<double>> anisotropic; //vektor na vysledky toho anizotropneho vypoctu
+	int N = 0;
+	double theta = 0.0;
+	double D[2][2];
+
+
+public:
+	Image() {};
+	Image(uchar* data, int w, int h, int bytesPerLine);
+
+	int getwidth() { return width; }
+	int getheight() { return height; }
+
+	double* getImageData() { return imageData.data(); }
+	uint8_t* getMask() { return mask.data(); }
+	double* getDamaged() { return damaged.data(); }
+	double* getLaplace() { return laplace.data(); }
+	double* getSmoothed() { return smoothed.data(); }
+
+	void generateMask(int p);
+	void Laplace();
+	void Smooth(double lambda);
+
+	void setN (int n) { N = n; }
+	void setTheta(double t) { theta = t; }
+	void Anisotropic();
+	void setD();
+};
